4aec505b7b931b11d12445bf5c6bfcf3458c053c
Rebecca French <rlfweb8@gmail.com>
2020-07-08 09:51:47 +0100

    ACF pro update

--- wp-content/themes/nomadsun/template-parts/content.php
+++ wp-content/themes/nomadsun/template-parts/content.php
@@ -11,10 +11,39 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<header class="entry-header">
-	</header><!-- .entry-header -->
+	<!-- <header class="entry-header">
+	</header>.entry-header -->
 
 	<div class="entry-content">
+		<!-- if we have some flexible content, let’s loop through it -->
+			<?php if( have_rows('content') ): while ( have_rows('content') ) : the_row();
+  // if it’s a header, go through the data
+			if( get_row_layout() == 'header' ): ?>
+
+			<!-- header	-->
+	<div> 
+			<!-- header image -->
+		<div class="w-60 min-vh 100 bg-blue">
+
+		</div>
+			<!-- header content -->
+		<div>
+	
+		</div>
+	</div>
+				<!-- data shared from our hero -->
+				<?php the_field('hero_image'); ?>
+				<?php the_field('date'); ?>
+				<!-- standard wordpress data -->
+				<?php the_title(); ?>
+				<?php the_field('subhead'); ?>
+				<!-- specific to this component -->
+				<?php the_sub_field('header_intro'); ?>
+			<!-- if it’s a text component, show us the data -->
+			<?php elseif( get_row_layout() == 'text_block' ): ?>
+				<?php the_sub_field('text_content'); ?>
+			<?php endif; 
+		endwhile; endif; ?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
